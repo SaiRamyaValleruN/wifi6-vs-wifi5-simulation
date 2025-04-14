nsnam/ns-3-dev-git

wifi: Add helper functions to HE RU class to reduce code duplication

src/wifi/model/he/he-ru.cc
@@ -10,6 +10,7 @@
 
 #include "ns3/abort.h"
 #include "ns3/assert.h"
+#include "ns3/wifi-ru.h"
 
 #include <optional>
 #include <tuple>
@@ -455,6 +456,35 @@ HeRu::RuSpec::GetPhyIndex(MHz_t bw, uint8_t p20Index) const
     {
         return m_index + GetNRus(bw, m_ruType) / 2;
     }
+}
+
+bool
+HeRu::GetPrimary80MHzFlag(MHz_t bw, RuType ruType, std::size_t phyIndex, uint8_t p20Index)
+{
+    if (bw < MHz_t{160} || ruType == RuType::RU_2x996_TONE)
+    {
+        return true;
+    }
+    const auto primary80IsLower80 = (p20Index < bw / MHz_t{40});
+    const auto indicesPer80MHz = GetNRus(MHz_t{80}, ruType);
+    return ((primary80IsLower80 && (phyIndex <= indicesPer80MHz)) ||
+            (!primary80IsLower80 && (phyIndex > indicesPer80MHz)));
+}
+
+std::size_t
+HeRu::GetIndexIn80MHzSegment(MHz_t bw, RuType ruType, std::size_t phyIndex)
+{
+    std::size_t index{phyIndex};
+    const auto indicesPer80MHz = GetNRus(MHz_t{80}, ruType);
+    if (WifiRu::GetBandwidth(ruType) > MHz_t{80})
+    {
+        index = 1;
+    }
+    else if (bw > MHz_t{80} && phyIndex > indicesPer80MHz)
+    {
+        index = (((phyIndex - 1) % indicesPer80MHz) + 1);
+    }
+    return index;
 }
 
 std::size_t

src/wifi/model/he/he-ru.h
@@ -106,6 +106,31 @@ class HeRu
     };
 
     /**
+     * Get the primary 80 MHz flag of a given RU transmitted in a PPDU.
+     * The flag identifies whether the RU is in the primary 80 MHz.
+     *
+     * @param bw the bandwidth of the PPDU (20, 40, 80, 160)
+     * @param ruType the RU type (number of tones)
+     * @param phyIndex the PHY index (starting at 1) of the RU
+     * @param p20Index the index of the primary20 channel
+     * @return the primary 80 MHz flag
+     */
+    static bool GetPrimary80MHzFlag(MHz_t bw,
+                                    RuType ruType,
+                                    std::size_t phyIndex,
+                                    uint8_t p20Index);
+
+    /**
+     * Get the index of a given RU transmitted in a PPDU within its 80 MHz segment.
+     *
+     * @param bw the bandwidth of the PPDU (20, 40, 80, 160)
+     * @param ruType the RU type (number of tones)
+     * @param phyIndex the PHY index (starting at 1) of the RU
+     * @return the index within the 80 MHz segment
+     */
+    static std::size_t GetIndexIn80MHzSegment(MHz_t bw, RuType ruType, std::size_t phyIndex);
+
+    /**
      * Get the number of distinct RUs of the given type (number of tones)
      * available in a HE PPDU of the given bandwidth.
      *

src/wifi/test/wifi-mac-ofdma-test.cc
@@ -325,17 +325,18 @@ TestMultiUserScheduler::ComputeWifiTxVector()
         NS_ABORT_MSG("Unsupported channel width");
     }
 
-    bool primary80 = true;
-    std::size_t ruIndex = 1;
-
+    auto primary80{true};
+    std::size_t ruIndex{1};
     for (auto& sta : staList)
     {
-        if (bw == MHz_t{160} && ruIndex == 3)
+        auto index{ruIndex};
+        if ((bw == MHz_t{160}) && (ruIndex == 3))
         {
-            ruIndex = 1;
-            primary80 = false;
+            index = HeRu::GetIndexIn80MHzSegment(bw, ruType, ruIndex);
+            primary80 = HeRu::GetPrimary80MHzFlag(bw, ruType, ruIndex, 0);
         }
-        m_txVector.SetHeMuUserInfo(sta.first, {HeRu::RuSpec{ruType, ruIndex++, primary80}, 11, 1});
+        m_txVector.SetHeMuUserInfo(sta.first, {HeRu::RuSpec{ruType, index, primary80}, 11, 1});
+        ruIndex++;
     }
     m_txVector.SetSigBMode(VhtPhy::GetVhtMcs5());
 }

src/wifi/test/wifi-primary-channels-test.cc
@@ -822,8 +822,16 @@ WifiPrimaryChannelsTest::SendDlMuPpdu(uint8_t bss,
 
     for (std::size_t i = 1; i <= nRus; i++)
     {
-        bool primary80 = !(txChannelWidth == MHz_t{160} && i > nRus / 2);
-        std::size_t index = (primary80 ? i : i - nRus / 2);
+        auto primary80{true};
+        auto index{i};
+        if ((txChannelWidth == MHz_t{160}) && (i > nRus / 2))
+        {
+            index = HeRu::GetIndexIn80MHzSegment(txChannelWidth, ruType, i);
+            primary80 = HeRu::GetPrimary80MHzFlag(txChannelWidth,
+                                                  ruType,
+                                                  i,
+                                                  apDev->GetPhy()->GetPrimary20Index());
+        }
 
         auto staDev = DynamicCast<WifiNetDevice>(m_staDevices[bss].Get(i - 1));
         uint16_t staId = DynamicCast<StaWifiMac>(staDev->GetMac())->GetAssociationId();
@@ -897,8 +905,16 @@ WifiPrimaryChannelsTest::DoSendHeTbPpdu(uint8_t bss,
                                << txChannelWidth << " channel an HE TB PPDU (RU type: " << ruType
                                << ")");
 
-        bool primary80 = !(txChannelWidth == MHz_t{160} && i > nRus / 2);
-        std::size_t index = (primary80 ? i : i - nRus / 2);
+        auto primary80{true};
+        auto index{i};
+        if ((txChannelWidth == MHz_t{160}) && (i > nRus / 2))
+        {
+            index = HeRu::GetIndexIn80MHzSegment(txChannelWidth, ruType, i);
+            primary80 = HeRu::GetPrimary80MHzFlag(txChannelWidth,
+                                                  ruType,
+                                                  i,
+                                                  apDev->GetPhy()->GetPrimary20Index());
+        }
 
         auto staDev = DynamicCast<WifiNetDevice>(m_staDevices[bss].Get(i - 1));
         uint16_t staId = DynamicCast<StaWifiMac>(staDev->GetMac())->GetAssociationId();

SaiRamyaValleruN/wifi6-vs-wifi5-simulation

src/wifi/model/he/he-ru.cc
@@ -10,6 +10,7 @@
 
 #include "ns3/abort.h"
 #include "ns3/assert.h"
+#include "ns3/wifi-ru.h"
 
 #include <optional>
 #include <tuple>
@@ -455,6 +456,35 @@ HeRu::RuSpec::GetPhyIndex(MHz_u bw, uint8_t p20Index) const
     {
         return m_index + GetNRus(bw, m_ruType) / 2;
     }
+}
+
+bool
+HeRu::GetPrimary80MHzFlag(MHz_u bw, RuType ruType, std::size_t phyIndex, uint8_t p20Index)
+{
+    if (bw < MHz_u{160} || ruType == RuType::RU_2x996_TONE)
+    {
+        return true;
+    }
+    const auto primary80IsLower80 = (p20Index < bw / MHz_u{40});
+    const auto indicesPer80MHz = GetNRus(MHz_u{80}, ruType);
+    return ((primary80IsLower80 && (phyIndex <= indicesPer80MHz)) ||
+            (!primary80IsLower80 && (phyIndex > indicesPer80MHz)));
+}
+
+std::size_t
+HeRu::GetIndexIn80MHzSegment(MHz_u bw, RuType ruType, std::size_t phyIndex)
+{
+    std::size_t index{phyIndex};
+    const auto indicesPer80MHz = GetNRus(MHz_u{80}, ruType);
+    if (WifiRu::GetBandwidth(ruType) > MHz_u{80})
+    {
+        index = 1;
+    }
+    else if (bw > MHz_u{80} && phyIndex > indicesPer80MHz)
+    {
+        index = (((phyIndex - 1) % indicesPer80MHz) + 1);
+    }
+    return index;
 }
 
 std::size_t

src/wifi/model/he/he-ru.h
@@ -106,6 +106,31 @@ class HeRu
     };
 
     /**
+     * Get the primary 80 MHz flag of a given RU transmitted in a PPDU.
+     * The flag identifies whether the RU is in the primary 80 MHz.
+     *
+     * @param bw the bandwidth of the PPDU (20, 40, 80, 160)
+     * @param ruType the RU type (number of tones)
+     * @param phyIndex the PHY index (starting at 1) of the RU
+     * @param p20Index the index of the primary20 channel
+     * @return the primary 80 MHz flag
+     */
+    static bool GetPrimary80MHzFlag(MHz_u bw,
+                                    RuType ruType,
+                                    std::size_t phyIndex,
+                                    uint8_t p20Index);
+
+    /**
+     * Get the index of a given RU transmitted in a PPDU within its 80 MHz segment.
+     *
+     * @param bw the bandwidth of the PPDU (20, 40, 80, 160)
+     * @param ruType the RU type (number of tones)
+     * @param phyIndex the PHY index (starting at 1) of the RU
+     * @return the index within the 80 MHz segment
+     */
+    static std::size_t GetIndexIn80MHzSegment(MHz_u bw, RuType ruType, std::size_t phyIndex);
+
+    /**
      * Get the number of distinct RUs of the given type (number of tones)
      * available in a HE PPDU of the given bandwidth.
      *

src/wifi/test/wifi-mac-ofdma-test.cc
@@ -325,17 +325,18 @@ TestMultiUserScheduler::ComputeWifiTxVector()
         NS_ABORT_MSG("Unsupported channel width");
     }
 
-    bool primary80 = true;
-    std::size_t ruIndex = 1;
-
+    auto primary80{true};
+    std::size_t ruIndex{1};
     for (auto& sta : staList)
     {
-        if (bw == MHz_u{160} && ruIndex == 3)
+        auto index{ruIndex};
+        if ((bw == MHz_u{160}) && (ruIndex == 3))
         {
-            ruIndex = 1;
-            primary80 = false;
+            index = HeRu::GetIndexIn80MHzSegment(bw, ruType, ruIndex);
+            primary80 = HeRu::GetPrimary80MHzFlag(bw, ruType, ruIndex, 0);
         }
-        m_txVector.SetHeMuUserInfo(sta.first, {HeRu::RuSpec{ruType, ruIndex++, primary80}, 11, 1});
+        m_txVector.SetHeMuUserInfo(sta.first, {HeRu::RuSpec{ruType, index, primary80}, 11, 1});
+        ruIndex++;
     }
     m_txVector.SetSigBMode(VhtPhy::GetVhtMcs5());
 }

src/wifi/test/wifi-primary-channels-test.cc
@@ -822,8 +822,16 @@ WifiPrimaryChannelsTest::SendDlMuPpdu(uint8_t bss,
 
     for (std::size_t i = 1; i <= nRus; i++)
     {
-        bool primary80 = !(txChannelWidth == MHz_u{160} && i > nRus / 2);
-        std::size_t index = (primary80 ? i : i - nRus / 2);
+        auto primary80{true};
+        auto index{i};
+        if ((txChannelWidth == MHz_u{160}) && (i > nRus / 2))
+        {
+            index = HeRu::GetIndexIn80MHzSegment(txChannelWidth, ruType, i);
+            primary80 = HeRu::GetPrimary80MHzFlag(txChannelWidth,
+                                                  ruType,
+                                                  i,
+                                                  apDev->GetPhy()->GetPrimary20Index());
+        }
 
         auto staDev = DynamicCast<WifiNetDevice>(m_staDevices[bss].Get(i - 1));
         uint16_t staId = DynamicCast<StaWifiMac>(staDev->GetMac())->GetAssociationId();
@@ -897,8 +905,16 @@ WifiPrimaryChannelsTest::DoSendHeTbPpdu(uint8_t bss,
                                << txChannelWidth
                                << " MHz channel an HE TB PPDU (RU type: " << ruType << ")");
 
-        bool primary80 = !(txChannelWidth == MHz_u{160} && i > nRus / 2);
-        std::size_t index = (primary80 ? i : i - nRus / 2);
+        auto primary80{true};
+        auto index{i};
+        if ((txChannelWidth == MHz_u{160}) && (i > nRus / 2))
+        {
+            index = HeRu::GetIndexIn80MHzSegment(txChannelWidth, ruType, i);
+            primary80 = HeRu::GetPrimary80MHzFlag(txChannelWidth,
+                                                  ruType,
+                                                  i,
+                                                  apDev->GetPhy()->GetPrimary20Index());
+        }
 
         auto staDev = DynamicCast<WifiNetDevice>(m_staDevices[bss].Get(i - 1));
         uint16_t staId = DynamicCast<StaWifiMac>(staDev->GetMac())->GetAssociationId();
nsnam/ns-3-dev-git

wifi: Fix HeRu::DoesOverlap when the given set of RUs is made of a single RU occupying the whole 160 MHz width

src/wifi/model/he/he-ru.cc
@@ -613,6 +613,12 @@ HeRu::DoesOverlap(MHz_t bw, RuSpec ru, const std::vector<RuSpec>& v)
     SubcarrierGroup rangesRu = GetSubcarrierGroup(bw, ru.GetRuType(), ru.GetIndex());
     for (auto& p : v)
     {
+        // A 2x996-tone RU spans 160 MHz, hence it overlaps
+        if (bw == MHz_t{160} && p.GetRuType() == RuType::RU_2x996_TONE)
+        {
+            return true;
+        }
+
         if (ru.GetPrimary80MHz() != p.GetPrimary80MHz())
         {
             // the two RUs are located in distinct 80MHz bands

SaiRamyaValleruN/wifi6-vs-wifi5-simulation

src/wifi/model/he/he-ru.cc
@@ -631,6 +631,12 @@ HeRu::DoesOverlap(MHz_u bw, RuSpec ru, const std::vector<RuSpec>& v)
     SubcarrierGroup rangesRu = GetSubcarrierGroup(bw, ru.GetRuType(), ru.GetIndex());
     for (auto& p : v)
     {
+        // A 2x996-tone RU spans 160 MHz, hence it overlaps
+        if (bw == MHz_u{160} && p.GetRuType() == RuType::RU_2x996_TONE)
+        {
+            return true;
+        }
+
         if (ru.GetPrimary80MHz() != p.GetPrimary80MHz())
         {
             // the two RUs are located in distinct 80MHz bands